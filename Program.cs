def5f84096f6daf76ab7b185db9d719c1a56b2bb
johnkennethdayola@outlook.com <johnkennethdayola@outlook.com>
2024-10-24 21:01:17 +0800

    Updated index links and edit toast

--- Program.cs
+++ Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Subjects}/{action=Add}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
---
+++ wwwroot/js/editToast.js
@@ -0,0 +1,30 @@
+﻿function displayToast(alertMessage, reloadLocation) {
+    if (alertMessage) {
+        const toastMessageElement = document.getElementById('toastMessage');
+        const toastTitleElement = document.getElementById('toastTitle');
+        const toastSquare = document.getElementById('toastSquare');
+        const formElement = document.getElementById('editForm');
+
+        formElement.classList.add('disable-interaction');
+
+        if (alertMessage.includes("successfully")) {
+            toastSquare.style.backgroundColor = '#51d251';
+            toastTitleElement.innerText = "Complete!";
+        } else {
+            toastSquare.style.backgroundColor = '#d12525';
+            toastTitleElement.innerText = "Error!";
+        }
+
+        toastMessageElement.innerText = alertMessage;
+
+        const toastElement = document.getElementById('toast');
+        toastElement.style.display = 'block';
+        const toast = new bootstrap.Toast(toastElement);
+        toast.show();
+
+        toastElement.addEventListener('hide.bs.toast', function () {
+            formElement.classList.remove('disable-interaction');
+            if (reloadLocation) window.location.href = reloadLocation;
+        });
+    }
+}
